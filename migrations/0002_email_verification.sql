32888ca0952064cfd2afc959807b890441de1bbe
Devansh-S <ryugabuisness@gmail.com>
2026-09-26 13:11:34 +0530

    Remove redundant email verification migration

--- migrations/0002_email_verification.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE users ADD COLUMN email_verified INTEGER NOT NULL DEFAULT 1;
-ALTER TABLE users ADD COLUMN email_verification_token_hash TEXT;
-ALTER TABLE users ADD COLUMN email_verification_expires_at TEXT;
-ALTER TABLE users ADD COLUMN email_verification_sent_at TEXT;
